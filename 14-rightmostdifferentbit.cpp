c94f4fe96b03499eaee1578fef34438fe988cb76
Anshul Tripathi <tripathianshul06@gmail.com>
2023-12-19 22:51:02 +0530

    potd done

---
+++ 14-rightmostdifferentbit.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int num1, int num2)
+    {
+        int i=0;
+        while(i<=32)
+        {
+            int a=(1<<i)&num1;
+            int b=(1<<i)&num2;
+            if(a!=b)
+            return (i+1);
+
+            i++;
+        }
+       
+        return -1;
+
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+} 
+// } Driver Code Ends
